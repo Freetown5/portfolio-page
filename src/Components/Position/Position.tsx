2c956fd32d9d10a0a3d63811ace09a835db7452b
Isatu Conteh <iconteh@gmail.com>
2026-02-03 14:39:01 -0500

    added images to page and sized

--- src/Components/Position/Position.tsx
+++ src/Components/Position/Position.tsx
@@ -4,6 +4,7 @@ interface employmentEntry {
     company: string,
     position: string,
     dates: string,
+    logoUrl: string,
     description: string
 }
 
@@ -12,36 +13,42 @@ const employmentData = [
         company: "RVO Health",
         position: "Senior Software Engineer (UI)",
         dates: "October 2023 - November 2025",
+        logoUrl: "./newPage/images/company_logo_folder/rvo_health_logo.png",
         description: "A company built around helping users find information regarding their health. Their product portfolio includes companies like Healthline and products like Real appeal (used to help with weight loss) and Quit for Life (used to help users quit smoking)."
     },
     {
         company: "Rally Health/Optum Digital",
         position: "Sr. Software Engineer (UI)",
         dates: "August 2021 - October 2023",
+        logoUrl: "./newPage/images/company_logo_folder/optum_logo.png",
         description: "Rally health was a digital health and wellness company that was aquired by Optum Digital through UnitedHealth Group. Optum Digital, United Health Groups tech organization refers to the suite of digital tools, platforms and technology enabled services offered by Optum to simplify health care and increase patient engagement and streamline administrative tasks."
     },
     {
         company: "Inspire (ClinicaHealth)",
         position: "Front End Developer",
         dates: "December 2018 - July 2021",
+        logoUrl: "./newPage/images/company_logo_folder/inspire_logo.png",
         description: "Inspire is company that partners with health ailment focused organizations to provide online communication platforms and use it to inform research."
     },
     {
         company: "Capital One",
         position: "Software Engineer",
         dates: "February 2018 - November 2018",
+        logoUrl: "./newPage/images/company_logo_folder/cap_one_logo.png",
         description: "A major (top 10) bank holding company known for it's extensive credit card operations, consumer banking, and commercial lending services."
     },
     {
         company: "The Washington Post",
         position: "Developer",
         dates: "October 2016 - February 2018",
+        logoUrl: "./newPage/images/company_logo_folder/wapo_logo.png",
         description: "A major daily newspaper knon for it's in-depth coverage of national politics and federal government affairs."
     },
     {
         company: "Speak Agent",
         position: "Web Developer",
         dates: "August 2015 - October 2016",
+        logoUrl: "./newPage/images/company_logo_folder/speak_agent_logo.png",
         description: "An educational tech (EdTech) company that produces popular digital, research-based supplemental programs for grades K-8 that improve academic language, literacy and concept mastery in math and science."
     }
 ];
@@ -51,13 +58,18 @@ const Position: React.FC<{}> = () => {
     return (
         <>
             { Object.entries(employmentData).map(([key, value]) => (
-                <div key={key} className="my-8">
-                    <h3 className="text-2xl">{ value.company }</h3>
-                    <ul>
-                        <li><span className="text-[#860D59] font-bold text-lg">Position:</span> { value.position }</li>
-                        <li><span className="text-[#860D59] font-bold text-lg">Dates of Employment:</span> { value.dates }</li>
-                        <li><span className="text-[#860D59] font-bold text-lg">Company Description:</span> { value.description }</li>
-                    </ul>
+                <div key={key} className="flex my-8">
+                    <div className="w-4/5">
+                        <h3 className="text-2xl">{ value.company }</h3>
+                        <ul>
+                            <li><span className="text-[#860D59] font-bold text-lg">Position:</span> { value.position }</li>
+                            <li><span className="text-[#860D59] font-bold text-lg">Dates of Employment:</span> { value.dates }</li>
+                            <li><span className="text-[#860D59] font-bold text-lg">Company Description:</span> { value.description }</li>
+                        </ul>
+                    </div>
+                    <div className="w-1/5">
+                        <img width="125px" height="125px" src={value.logoUrl} alt="testing" />
+                    </div>
                 </div> 
             ))}
 
