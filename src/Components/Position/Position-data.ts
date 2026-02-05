7cdbecd3c2e6260faee5d51b209f5ded9a09b5ea
Isatu Conteh <iconteh@gmail.com>
2026-02-05 10:48:38 -0500

    added and scaffolded more data

--- src/Components/Position/Position-data.ts
+++ src/Components/Position/Position-data.ts
@@ -17,34 +17,34 @@ export const employmentData = [
             {
                 project: 'Coaching App (Q4L program)',
                 productType:'web app',
-                audience: 'United Health customer looking to ',
+                audience: 'Residents of states and companies with public health programs.',
                 description: '',
                 quip: '',
-                toolsUsed: ''
+                toolsUsed: 'Angular 12, RxJs, NgRx, Contentful etc.'
             },
             {
                 project: 'Quit For Life Landing Page',
                 productType:'web app',
-                audience: 'United Health customer looking to ',
+                audience: 'Residents of states and companies looking to manage and cut down on general tobacco usage.',
                 description: '',
                 quip: '',
-                toolsUsed: ''
+                toolsUsed: 'Next.JS (React), Typescript, Netlify, Github Actions, Google Translate, next-Intl, Tailwind CSS, Wordpress (old site), Javascript (old site)'
             },
             {
                 project: 'Live Vape Free Landing Page',
                 productType:'web app',
-                audience: 'United Health customer looking to ',
+                audience: 'Residents of states and companies looking to manage and cut down specifically on usage of vape products.',
                 description: '',
                 quip: '',
-                toolsUsed: ''
+                toolsUsed: 'Next.JS (React), Typescript, Tailwind CSS, Netlify, Github Actions'
             },
             {
                 project: 'Coaching Client Configurations App',
                 productType:'web app',
-                audience: 'United Health customer looking to ',
+                audience: 'Commercial companies and state clients looking to customize the coaching app for their customers and residents.',
                 description: '',
                 quip: '',
-                toolsUsed: ''
+                toolsUsed: 'React, Company UI Library, Redux'
             }
         ]
     },
@@ -53,34 +53,156 @@ export const employmentData = [
         position: "Sr. Software Engineer (UI)",
         dates: "August 2021 - October 2023",
         logoUrl: "./newPage/images/company_logo_folder/optum_logo.png",
-        description: "Rally health was a digital health and wellness company that was aquired by Optum Digital through UnitedHealth Group. Optum Digital, United Health Groups tech organization refers to the suite of digital tools, platforms and technology enabled services offered by Optum to simplify health care and increase patient engagement and streamline administrative tasks."
+        description: "Rally health was a digital health and wellness company that was aquired by Optum Digital through UnitedHealth Group. Optum Digital, United Health Groups tech organization refers to the suite of digital tools, platforms and technology enabled services offered by Optum to simplify health care and increase patient engagement and streamline administrative tasks.",
+        selectContent: [
+            {
+                project: 'Rally Design System (RDS)',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+            {
+                project: 'Orion Design System',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+            {
+                project: 'Better.UI Design System',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            }
+        ]
     },
     {
         company: "Inspire (ClinicaHealth)",
         position: "Front End Developer",
         dates: "December 2018 - July 2021",
         logoUrl: "./newPage/images/company_logo_folder/inspire_logo.png",
-        description: "Inspire is company that partners with health ailment focused organizations to provide online communication platforms and use it to inform research."
+        description: "Inspire is company that partners with health ailment focused organizations to provide online communication platforms and use it to inform research.",
+        selectContent: [
+            {
+                project: 'Component Library',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+            {
+                project: 'Resource Center',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+            {
+                project: 'Treatment Pages',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+            {
+                project: 'Search Page',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            }
+        ]
     },
     {
         company: "Capital One",
         position: "Software Engineer",
         dates: "February 2018 - November 2018",
         logoUrl: "./newPage/images/company_logo_folder/cap_one_logo.png",
-        description: "A major (top 10) bank holding company known for it's extensive credit card operations, consumer banking, and commercial lending services."
+        description: "A major (top 10) bank holding company known for it's extensive credit card operations, consumer banking, and commercial lending services.",
+        selectContent: [
+            {
+                project: 'Workbench',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+        ]
     },
     {
         company: "The Washington Post",
         position: "Developer",
         dates: "October 2016 - February 2018",
         logoUrl: "./newPage/images/company_logo_folder/wapo_logo.png",
-        description: "A major daily newspaper knon for it's in-depth coverage of national politics and federal government affairs."
+        description: "A major daily newspaper knon for it's in-depth coverage of national politics and federal government affairs.",
+        selectContent: [
+            {
+                project: 'Wapo Feeds',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+            {
+                project: 'Book Best Sellers App',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+            {
+                project: 'Notification Tool',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+            {
+                project: 'Elections App',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            }
+        ]
     },
     {
         company: "Speak Agent",
         position: "Web Developer",
         dates: "August 2015 - October 2016",
         logoUrl: "./newPage/images/company_logo_folder/speak_agent_logo.png",
-        description: "An educational tech (EdTech) company that produces popular digital, research-based supplemental programs for grades K-8 that improve academic language, literacy and concept mastery in math and science."
+        description: "An educational tech (EdTech) company that produces popular digital, research-based supplemental programs for grades K-8 that improve academic language, literacy and concept mastery in math and science.",
+        selectContent: [
+            {
+                project: '',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+            {
+                project: '',
+                productType:'',
+                audience: '',
+                description: '',
+                quip: '',
+                toolsUsed: ''
+            },
+        ]
     }
 ];
